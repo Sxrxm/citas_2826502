b464a847de34b468fe4a32cf5c27bf90eaab63cc
Danner1233 <dannerariasmunive11@gmail.com>
2024-03-06 15:08:55 -0500

    evidencia3

--- app/models.py
+++ app/models.py
@@ -47,4 +47,20 @@ class Cita(db.Model):
     paciente_id = db.Column(db.Integer, db.ForeignKey("pacientes.id"))
     medico_id = db.Column(db.Integer, db.ForeignKey("medicos.id"))
     consultorio_id = db.Column(db.Integer, db.ForeignKey("consultorios.id")) 
+    valor  = db.Column (db.Integer)
     
+    
+    
+    ## from app.models import Medico,Paciente,Cita,Consultorio 
+
+## me2=Medico(nombre="Juliana", apellido="Osorio",tipo_identificacion="CC",numero_identificacion=9876543,especialidad="odontologia") 
+## me1=Medico(nombre="Sofia", apellido="Gomez",tipo_identificacion="CC",numero_identificacion=87654,especialidad="Pediatra")
+## me3=Medico(nombre="Pedro", apellido="Ortiz",tipo_identificacion="CC",numero_identificacion=2345643,especialidad="pediatra") 
+## p1=Paciente(nombre="Milena",apellido="Arias",tipo_identificacion="TI",altura=160,tipo_sangre="A+")
+## p2=Paciente(nombre="Julio",apellido="Rodriguez",tipo_identificacion="TI",altura=170,tipo_sangre="O+")  
+## p3=Paciente(nombre="Nelson",apellido="Romero",tipo_identificacion="CC",altura=180,tipo_sangre="O+")   
+## c1=Consultorio(numero=301)
+## c2=Consultorio(numero=302) 
+## c3=Consultorio(numero=303) 
+##for ci in Cita.query.all():                                                                                                              
+##     print("fecha:"+ str(c1.fecha)+"paciente identificacion:"+ str(ci.paciente.tipo_identificacion)+",paciente nombre:"+(ci.paciente.nombre)+"paciente apellido:"+( ci.paciente.apellido) + "registro medico:"+str(ci.medico.registro_medico) + "consultorio:" + str(ci.consultorio.numero) + "valor cita:"+str(ci.valor)) 
